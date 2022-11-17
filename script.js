9fb323f8da1be0c9857f6d4ed007bf7fa1030208
luisina gonzalez <luisinaggiacoia@gmail.com>
2022-11-17 20:11:28 -0300

    finishedjs

---
+++ script.js
@@ -0,0 +1,19 @@
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel =>{
+    panel.addEventListener('click', ()=>{
+        removeActionClasses()
+        panel.classList.add('active')
+    })
+})
+
+// function removeActionClasses(){
+//     panels.forEach(panel => {
+//         panel.classList.remove('active')
+//     })
+// }
+function removeActionClasses(){
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
